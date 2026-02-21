e37dfa8cfaa3bcd281224f33e53136110d87819a
Niteshmishra <atrangifacts036@gmail.com>
2026-02-21 23:48:55 +0530

    Fixed resend sender + debug log

--- server.js
+++ server.js
@@ -9,55 +9,53 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://yourdomain.com"
-  ]
+    origin: "*"
 }));
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 app.post("/api/book", async (req, res) => {
-  try {
-    const {
-      fullName,
-      email,
-      phone,
-      department,
-      doctor,
-      date,
-      time,
-      message,
-    } = req.body;
+    try {
+        const {
+            fullName,
+            email,
+            phone,
+            department,
+            doctor,
+            date,
+            time,
+            message,
+        } = req.body;
 
-    const bookingId = "APX-" + Math.floor(100000 + Math.random() * 900000);
+        const bookingId = "APX-" + Math.floor(100000 + Math.random() * 900000);
 
-    await resend.emails.send({
-      from: "appointments@yourdomain.com",
-      to: "yourhospitalemail@gmail.com",
-      subject: `New Appointment - ${bookingId}`,
-      html: `
-        <h2>New Appointment Booking</h2>
-        <p><strong>Booking ID:</strong> ${bookingId}</p>
-        <p><strong>Name:</strong> ${fullName}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Phone:</strong> ${phone}</p>
-        <p><strong>Department:</strong> ${department}</p>
-        <p><strong>Doctor:</strong> ${doctor}</p>
-        <p><strong>Date:</strong> ${date}</p>
-        <p><strong>Time:</strong> ${time}</p>
-        <p><strong>Message:</strong> ${message || "Not Provided"}</p>
-      `,
-    });
+        const result = await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: "nitinmishra28a@gmail.com",
+            subject: `New Appointment - ${bookingId}`,
+            html: `
+              <h2>New Appointment Booking</h2>
+              <p><strong>Booking ID:</strong> ${bookingId}</p>
+              <p><strong>Name:</strong> ${fullName}</p>
+              <p><strong>Email:</strong> ${email}</p>
+              <p><strong>Phone:</strong> ${phone}</p>
+              <p><strong>Department:</strong> ${department}</p>
+              <p><strong>Doctor:</strong> ${doctor}</p>
+              <p><strong>Date:</strong> ${date}</p>
+              <p><strong>Time:</strong> ${time}</p>
+              <p><strong>Message:</strong> ${message || "Not Provided"}</p>
+            `,
+        });
+        console.log("Resend response:", result);
 
-    res.json({ success: true, bookingId });
+        res.json({ success: true, bookingId });
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false });
-  }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false });
+    }
 });
 
 app.listen(process.env.PORT || 5000, () =>
-  console.log("Server running")
+    console.log("Server running")
 );
